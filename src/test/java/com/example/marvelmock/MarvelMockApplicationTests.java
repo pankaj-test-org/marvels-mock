6a4a39eeb6c47b6f6520f928df2c884e5115eced
Pankaj Yadav <pyadav@cloudbees.com>
2025-12-05 13:32:13 +0530

    Add .gitignore, initial test classes, and local settings configuration

---
+++ src/test/java/com/example/marvelmock/MarvelMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marvelmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
